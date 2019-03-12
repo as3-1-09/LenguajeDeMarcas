6983214791bde41bd5d30c3e7ccb0a24a2d1f4dc
AS3-1-09 <AS3-1-09@uni.lan>
2019-03-12 12:59:27 +0100

    nodejs

--- nodejs/ejemplo01/ejemplo.js
+++ nodejs/ejemplo01/ejemplo.js
@@ -1,3 +1,33 @@
 /*
-ejemplo 01
+ejemplo 01: http server. read x and y and return sum
 */
+
+
+// https://www.npmjs.com/package/http
+
+/*
+x = 5
+y = 6
+
+z = x + y
+*/
+
+z = Math.floor(Math.random() * 51);
+
+// si la suma es > 10. Print "es mayor que 10"
+// si no: "es menor que 10"
+// si = 10: "es igual a 10"
+// pista: usa "if / else"
+
+// console.log(z)
+
+console.log("el valor de z es: " + z)
+if (z > 10) {
+    //true
+    console.log("es mayor que 10")
+} else if (z < 10) {
+    //true
+    console.log("es menor que 10")
+} else {
+    console.log("es igual a 10")
+}
