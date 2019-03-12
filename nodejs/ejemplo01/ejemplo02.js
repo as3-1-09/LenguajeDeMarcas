c06f21ef3d1ba36d5b83ee458edfeb4c3421b022
AS3-1-09 <AS3-1-09@uni.lan>
2019-03-12 13:18:59 +0100

    nodejs

--- nodejs/ejemplo01/ejemplo02.js
+++ nodejs/ejemplo01/ejemplo02.js
@@ -5,7 +5,7 @@ ejemplo 02(while)
 //ahora, repetir hasta que z ==10
 z = 0;
 while (z != 10) {
-    z = Math.floor(Math.random() * 51);
+    z = Math.floor(Math.random() * 1000);
     console.log("el valor de z es: " + z)
 
     // https://www.w3schools.com/js/js_if_else.asp
