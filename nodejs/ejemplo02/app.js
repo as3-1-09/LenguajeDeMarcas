9725fbdea466718da732f228225375543ed86445
AS3-1-09 <AS3-1-09@uni.lan>
2019-04-02 13:18:02 +0500

    nodejs

--- nodejs/ejemplo02/app.js
+++ nodejs/ejemplo02/app.js
@@ -9,6 +9,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
+app.get('/info', (req, res) => res.send('INFORMACION'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
